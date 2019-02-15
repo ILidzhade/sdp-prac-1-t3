3ed3e2265e64fd18c474d1c01e026f22c5c41504
Lesedi Pitsi <G17P9239@ict.ru.ac.za>
2019-02-15 20:56:00 +0200

    added isPrime method -Ipfi

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -11,5 +11,22 @@ namespace Euler
         static void Main(string[] args)
         {
         }
+
+        public bool isPrime(int x)
+        {
+            int n = 1;
+            int k = 2;
+            while (n <= 2 && k <= x/2)
+            {
+                if (x % k == 0)
+                {
+                    k++;
+                    n++;
+                }
+                else k++;
+            }
+            if (n == 2) return true;
+            else return false;
+        }
     }
 }
